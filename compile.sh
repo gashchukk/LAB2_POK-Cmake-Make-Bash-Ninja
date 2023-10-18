7e04c358f178ba2685343b47420ee37558c48ea2
lizaArnauta <arnauta.pn@ucu.edu.ua>
2023-10-18 11:38:31 +0200

    compile sh file update

--- compile.sh
+++ compile.sh
@@ -8,39 +8,43 @@
 # fi
 
 # create dynamic lib
-g++ -std=c++11 -shared -fPIC -o library/mystring.dylib library/mystring.cpp
+
+g++ mystring/library/mystring.cpp -o obj/mystring.o -c -O3
+g++ mystring/examples/main.cpp -o obj/main.o -c -O3
+
+ar rcs mystring/library/libmystring.a obj/mystring.o
+
+g++ obj/main.o -lmystring -L./mystring/library/
+./a.out
+#
+##g++ -std=c++11 -shared -fPIC -o mystring/library/mystring.so obj/mystring.o
 if [ $? -eq 0 ]; then
     echo "Dynamic library compiled successfully."
 else
     echo "Dynamic library compilation failed."
     exit 1
 fi
-#rm library/mystring.dylib
-
-# create static lib
-g++ -std=c++11 -c -o library/mystring.o library/mystring.cpp
+##rm library/mystring.so
+#
+## create static lib
+#g++ -std=c++11 -c -o library/mystring.o library/mystring.cpp
 if [ $? -eq 0 ]; then
     echo "Static library compiled successfully."
 else
     echo "Static library compilation failed."
     exit 1
 fi
-
-# ar is the archiver command. rcs are options for creating the archive, replacing existing files, and creating an index.
-ar rcs library/mystring.a library/mystring.o mystring.dylib
-rm library/mystring.o
-rm library/mystring.a
-
-# example files (exe?)
-g++ -std=c++11 -o library/mystring library/mystring.dylib -Llibrary -lmystring
+#
+## ar is the archiver command. rcs are options for creating the archive, replacing existing files, and creating an index.
+#ar rcs mystring/library/mystring.a mystring/library/mystring.o mystring.so
+##rm library/mystring.o
+##rm library/mystring.a
+#
+## example files (exe?)
+#g++ -std=c++11 -o mystring/library/mystring library/mystring.so -Llibrary -lmystring
 if [ $? -eq 0 ]; then
     echo "Examples compiled successfully."
 else
     echo "Examples compilation failed."
     exit 1
 fi
-
-# код працює лише під мак. виникиє помилка:
-#ld: library not found for -lmystring
-#clang: error: linker command failed with exit code 1 (use -v to see invocation)
-#Examples compilation failed.
