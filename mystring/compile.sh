cd56e17d2e04f84e38908e08f1f131884c94d6c0
shuma <shuma@ubuntu.myguest.virtualbox.org>
2023-12-08 22:47:52 +0200

    final fix

--- mystring/compile.sh
+++ mystring/compile.sh
@@ -8,13 +8,13 @@ mkdir -p lib
 
 # Compile source files into object files
 g++ -std=c++17 -fPIC -c library/mystring.cpp -o obj/mystring.o
-g++ -std=c++17 -fPIC -c examples/main.cpp -o obj/main.o
+g++ -std=c++17 -fPIC -c examples/main.cpp -o obj/main.o -Ilibrary
 
 # Create dynamic lib
 g++ -shared obj/mystring.o obj/main.o -o lib/libmystring.so
 
 # Link obj to bin
-g++ -std=c++17 -L./lib -lmystring obj/main.o -o bin/myprogram_dynamic -Wl,-rpath,lib
+g++ -std=c++17 -L./lib -lmystring obj/main.o -o bin/myprogram_dynamic -Wl,-rpath,lib -Ilibrary
 
 
 #### Static ####
@@ -22,4 +22,5 @@ g++ -std=c++17 -L./lib -lmystring obj/main.o -o bin/myprogram_dynamic -Wl,-rpath
 ar rcs lib/libmystring.a obj/mystring.o obj/main.o
 
 # Create the Executable Linked with the Static Library
-g++ -std=c++17 -L./lib -lmystring obj/main.o -o bin/myprogram_static
+# g++ -std=c++17 -lmystring obj/main.o -o bin/myprogram_static -Llib
+g++ -std=c++17 obj/main.o -o bin/myprogram_static lib/libmystring.a
